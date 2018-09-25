fff195c824bebabe5b1ae101c30a24d4ae703e2c
Juan Matute <juan.matute@gmail.com>
2018-09-25 00:23:56 -0400

    express site

---
+++ lib/controllers/caseController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+export class CaseController{
+
+    public addNewContact (req: Request, res: Response) {                
+
+    }
+
+    public getContacts (req: Request, res: Response) {           
+
+    }
+
+    public getContactWithID (req: Request, res: Response) {           
+
+    }
+
+    public updateContact (req: Request, res: Response) {           
+
+    }
+
+    public deleteContact (req: Request, res: Response) {           
+
+    }
+    
+}
